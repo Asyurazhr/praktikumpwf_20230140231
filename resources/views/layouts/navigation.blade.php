ea49d34ec0f140669025f1950dc1312bd4bae7db
Asyurazhr <Asyuradjola2004@gmail.com>
2026-05-09 23:35:06 +0700

    Praktikum5

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,10 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+    
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        
         <div class="flex justify-between h-16">
+
             <div class="flex">
                 
                 <!-- Logo -->
@@ -24,17 +27,30 @@
                         {{ __('About') }}
                     </x-nav-link>
 
+                    <!-- Product -->
+                    @can('manage-product')
+
+                    <x-nav-link href="#">
+                        {{ __('Product') }}
+                    </x-nav-link>
+
+                    @endcan
+
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+
                 <x-dropdown align="right" width="48">
 
                     <x-slot name="trigger">
+
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
 
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>
+                                {{ Auth::user()->name }} ({{ Auth::user()->role }})
+                            </div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4"
@@ -44,10 +60,12 @@
                                     <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                         clip-rule="evenodd" />
+
                                 </svg>
                             </div>
 
                         </button>
+
                     </x-slot>
 
                     <x-slot name="content">
@@ -65,6 +83,7 @@
                                 this.closest('form').submit();">
 
                                 {{ __('Log Out') }}
+
                             </x-dropdown-link>
 
                         </form>
@@ -72,6 +91,7 @@
                     </x-slot>
 
                 </x-dropdown>
+
             </div>
 
             <!-- Hamburger -->
@@ -104,6 +124,7 @@
                 </button>
 
             </div>
+
         </div>
     </div>
 
@@ -126,13 +147,15 @@
         <div class="pt-4 pb-1 border-t border-gray-200">
 
             <div class="px-4">
+
                 <div class="font-medium text-base text-gray-800">
-                    {{ Auth::user()->name }}
+                    {{ Auth::user()->name }} ({{ Auth::user()->role }})
                 </div>
 
                 <div class="font-medium text-sm text-gray-500">
                     {{ Auth::user()->email }}
                 </div>
+
             </div>
 
             <div class="mt-3 space-y-1">
@@ -150,6 +173,7 @@
                         this.closest('form').submit();">
 
                         {{ __('Log Out') }}
+
                     </x-responsive-nav-link>
 
                 </form>
@@ -157,5 +181,7 @@
             </div>
 
         </div>
+
     </div>
+
 </nav>
